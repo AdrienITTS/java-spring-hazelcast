90ee1c1a8fa1081a72c87e14cac662aa8fdf44a9
AdrienITTS <adrien.deprez@dewizz.com>
2021-05-22 23:40:00 +0200

    feat: initialize project from spring initializr

---
+++ src/test/java/com/demo/hazelcast/demoHazelcast/DemoHazelcastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.hazelcast.demoHazelcast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoHazelcastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
